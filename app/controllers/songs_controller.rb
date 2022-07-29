180dc5a3edac8811587d65fff8ffe7ccd0915d1b
max <max.lillie95@gmail.com>
2022-07-29 13:09:10 -0500

    added dynamic show, and stagnent create options, and started the skeleton for patch

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,21 @@ class SongsController < ApplicationController
     songs = Song.all
     render json: songs.as_json
   end
+
+  def show
+    p id: params[:id]
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+    # render json: { message: "You remembered!" }
+  end
+
+  def create
+    song = Song.new(title: "The Safety Dance", albunm: "Rhythm of Youth", artist: "Men Without Hats", year: "1982")
+    song.save
+    # render json: { message: "2/3" }
+  end
+
+  def update
+    render json: { message: "getting this habbit now" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   #get get R patch delete
   get "/my_songs" => "songs#index"
+  get "/my_songs/:id" => "songs#show"
+  post "/my_songs" => "songs#create"
+  patch "my_songs/:id" => "songs#update"
 end

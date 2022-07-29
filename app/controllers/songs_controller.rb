216f9de256891ef5d1216fc76750b05763e998e2
max <max.lillie95@gmail.com>
2022-07-29 12:52:25 -0500

    added index route, attempted to fix wrong key in database names

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,7 @@
 class SongsController < ApplicationController
+  def index
+    # render json: { message: "stop forggeting this part you cyka" }
+    songs = Song.all
+    render json: songs.as_json
+  end
 end
--- db/migrate/20220729173506_create_songs.rb
+++ db/migrate/20220729173506_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[7.0]
   def change
     create_table :songs do |t|
       t.string :title
-      t.string :albunm
+      t.string :album
       t.string :artist
       t.integer :year
 
